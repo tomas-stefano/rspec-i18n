d09c2994dff92d844f879d6b6faff28b4d0c8fc9
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-01-09 17:23:35 -0200

    Add the be_an_instance_of matcher translated i18n

--- lib/spec-i18n/matchers.rb
+++ lib/spec-i18n/matchers.rb
@@ -1,5 +1,6 @@
 require 'spec-i18n/matchers/be'
 require 'spec-i18n/matchers/be_close'
+require 'spec-i18n/matchers/be_instance_of'
 require 'spec-i18n/matchers/eql'
 require 'spec-i18n/matchers/equal'
 require 'spec-i18n/matchers/method_missing'
---
+++ spec/spec-i18n/matchers/be_instance_of_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+module Spec
+  module Matchers
+    describe 'the be_instance_of method' do
+      
+      before(:each) do
+        @expected_matcher = { 'matchers' => { 'be_an_instance_of' => 'ser_instancia_de'} }
+        portuguese_language(@expected_matcher)
+        Spec::Matchers.register_be_an_instance_of_matcher
+      end
+      
+      it "register the be_an_instance_of method" do
+        values = @expected_matcher['matchers']['be_an_instance_of'].split('|')
+        values.each do |value_method|
+          Object.instance_methods.should be_include(value_method)
+        end
+      end
+      
+      it "passes if actual is instance of expected class" do
+        5.should send(:ser_instancia_de, Fixnum)
+      end
+      
+      it "fails if actual is instance of subclass of expected class" do
+        lambda { 
+          5.should send(:ser_instancia_de, Numeric)          
+        }.should raise_error
+      end
+      
+    end
+  end
+end
