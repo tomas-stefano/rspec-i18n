aeba77d160ebc81b5ce346e53418972da7c3852a
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-01-09 16:58:46 -0200

    Add the be_close matcher

--- lib/spec-i18n/matchers.rb
+++ lib/spec-i18n/matchers.rb
@@ -1,4 +1,5 @@
 require 'spec-i18n/matchers/be'
+require 'spec-i18n/matchers/be_close'
 require 'spec-i18n/matchers/eql'
 require 'spec-i18n/matchers/equal'
 require 'spec-i18n/matchers/method_missing'
---
+++ spec/spec-i18n/matchers/be_close_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+module Spec
+  module Matchers
+    describe "[actual.should] be_close(expected, delta)" do
+      
+      before(:each) do
+        @expected_matcher = {'matchers' => { 'be_close' => 'estar_perto'} }
+        portuguese_language(@expected_matcher)
+        Spec::Matchers.register_be_close_matcher
+      end
+      
+      it "should register the be_close matcher" do
+        values = @expected_matcher['matchers']['be_close'].split('|')
+        values.each do |value_method|
+          Object.instance_methods.should be_include(value_method)
+        end
+      end
+      
+      it "matches when actual == expected" do
+        estar_perto(5.0, 0.5).matches?(5.0).should be_true
+      end
+      
+      it "matches when actual < (expected + delta)" do
+        estar_perto(5.0, 0.5).matches?(5.49).should be_true
+      end
+      
+      it "does not match when actual == (expected - delta)" do
+        estar_perto(5.0, 0.5).matches?(4.5).should be_false
+      end
+      it "does not match when actual < (expected - delta)" do
+        estar_perto(5.0, 0.5).matches?(4.49).should be_false
+      end
+      it "does not match when actual == (expected + delta)" do
+        estar_perto(5.0, 0.5).matches?(5.5).should be_false
+      end
+      
+    end
+  end
+end
