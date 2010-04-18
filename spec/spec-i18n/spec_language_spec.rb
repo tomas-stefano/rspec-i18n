3f3a76c3f26b2006003b8c5f0ac604a30b5e3607
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-04-17 23:27:36 -0300

    desnecessary include

--- spec/spec-i18n/spec_language_spec.rb
+++ spec/spec-i18n/spec_language_spec.rb
@@ -1,11 +1,7 @@
 require "spec_helper"
 
 describe SpecI18n do
-  
-  before(:each) do
-    include SpecI18n
-  end
-  
+    
   it "should assign the spec language constant" do
     languages = ["pt", "es"]
     languages.each do |language|
